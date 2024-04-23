791609b852f45d2be0022edefe4f72d746569a2e
justobello <justobello352@gmail.com>
2024-04-23 05:25:36 -0400

    cambios agrgados api

--- app/src/components/Inicio.jsx
+++ app/src/components/Inicio.jsx
@@ -2,12 +2,44 @@
 import Enlace from "./Enlace";
 import "./style/Inicio.css"
 import {CiUser, CiSettings} from "react-icons/ci"
+import { useState, useEffect } from "react";
 
 
 
 
 
 const Inicio = () => {
+
+   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [filterCriteria, setFilterCriteria] = useState('');
+
+  useEffect(() => {
+    // Llamada a la API para obtener los datos
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error fetching data');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setData(data);
+        setFilteredData(data); // Inicialmente, mostrar todos los datos
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const handleFilterChange = (e) => {
+    const { value } = e.target;
+    setFilterCriteria(value);
+    // Aplicar filtro a los datos
+    const filtered = data.filter(item => item.name.toLowerCase().includes(value.toLowerCase()));
+    setFilteredData(filtered);
+  };
+
 
     return ( 
 
@@ -21,7 +53,14 @@ const Inicio = () => {
    <li><a href="#">Estado</a></li>
   </ul>
   <section>
-   <input type="search" placeholder="  que estas buscando?" />
+  <div>
+      <input
+        type="text"
+        placeholder="Filtrar por nombre"
+        value={filterCriteria}
+        onChange={handleFilterChange}
+      />
+    </div>
   </section>
   <section className="user-home">
    <a href="#"> <CiUser /> </a>
@@ -31,6 +70,14 @@ const Inicio = () => {
 
 <Enlace />
 
+     <article className="container">
+     <ul>
+        {filteredData.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+
+     </article>
 
       </main>
 
